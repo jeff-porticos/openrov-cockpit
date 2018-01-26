4af47a854e2c7c10003f61354761327ce8746812
Neil Jacobson <neil@formidableengineeringconsultants.com>
2018-01-26 14:14:30 -0800

    To ensure that images are sharp on each tab, I tried to use emitted events but I couldn;t get the emitter to shared between orov-video and mjpeg-packet-video Instead inactive tabs send null frames to mjpeg-packet-video and set a stopped flag When they become active, a refresh is executed, the stopped flag is cleared and business continues

--- src/plugins/video/public/js/video-widgets-autodiscovery.js
+++ src/plugins/video/public/js/video-widgets-autodiscovery.js
@@ -8,22 +8,22 @@
   widgets['orov-video1'] = {
     name: 'orov-video1',
     defaultUISymantic: 'data-control-unit',
-    url: 'video/video1.html'
+    url: 'video/orov-video1.html'
   };
   widgets['orov-video2'] = {
     name: 'orov-video2',
     defaultUISymantic: 'data-control-unit',
-    url: 'video/video2.html'
+    url: 'video/orov-video2.html'
   };
   widgets['orov-video3'] = {
     name: 'orov-video3',
     defaultUISymantic: 'data-control-unit',
-    url: 'video/video3.html'
+    url: 'video/orov-video3.html'
   };
   widgets['orov-video4'] = {
     name: 'orov-video4',
     defaultUISymantic: 'data-control-unit',
-    url: 'video/video4.html'
+    url: 'video/orov-video4.html'
   };
 }  // The line below both ends the anonymous function and then calls
    // it passing in the required depenencies.
